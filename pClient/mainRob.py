62274dadc9b152c465d830086cb00f33031d0191
RaquelRainho <raquel.a.rainho@ua.pt>
2021-03-11 21:46:27 +0000

    Added robot movement

--- pClient/mainRob.py
+++ pClient/mainRob.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 
 CELLROWS=7
 CELLCOLS=14
+CELL_SIDE=2
 
 class MyRob(CRobLinkAngs):
     def __init__(self, rob_name, rob_id, angles, host):
@@ -25,7 +26,6 @@ class MyRob(CRobLinkAngs):
 
     def set_targets(self, targets):
         self.targets = targets
-        print targets
 
     def set_challenge(self, challenge):
         self.challenge = challenge
@@ -37,12 +37,28 @@ class MyRob(CRobLinkAngs):
 
         state = 'stop'
         stopped_state = 'run'
+        self.coords_offset = None
+        self.dest_cell = self.start_cell
+        self.dest_angle = 0
+
+        # get directions list (hardcoded for now)
+        self.path = [(2,3), (3,3), (3,4), (4,4), (4,5), (5,5), (6,5), (6,6), (7,6), (8,6), (9,6), (10,6), (11,6), (11,5), (11,4), (11,3), (12,3)]
+        self.path_index = 0
+        self.get_next_dest()
+
+        # number of targets visited
+        self.num_targets_visited = 0
+        on_target = False
 
         while True:
             self.readSensors()
 
+            if not self.coords_offset:
+                #calcular offset
+                self.coords_offset = (self.measures.x - self.transform_to_gps_pos(self.start_cell[0]), self.measures.y - self.transform_to_gps_pos(self.start_cell[1]))
+
             if self.measures.endLed:
-                print(self.rob_name + " exiting")
+                print(self.robName + " exiting")
                 quit()
 
             if state == 'stop' and self.measures.start:
@@ -55,22 +71,47 @@ class MyRob(CRobLinkAngs):
             if state == 'run':
                 if self.measures.visitingLed==True:
                     state='wait'
-                if self.measures.ground==0:
-                    self.setVisitingLed(True);
-                self.wander()
+                    self.num_targets_visited += 1
+                else:
+                    self.decide()
+                if self.measures.ground >= 0 and not on_target:
+                    self.setVisitingLed(True)
+                    on_target = True
+                elif self.measures.ground < 0 and on_target:
+                    on_target = False
+                
             elif state=='wait':
-                self.setReturningLed(True)
+                # return to initial position only if all targets were visited
+                if self.num_targets_visited == len(self.targets):
+                    self.setReturningLed(True)
+                else:
+                    state='run'
                 if self.measures.visitingLed==True:
                     self.setVisitingLed(False)
                 if self.measures.returningLed==True:
                     state='return'
-                self.driveMotors(0.0,0.0)
+                    self.num_targets_visited = 0
+                    on_target = False
+                    self.path_index = 0
+                    self.path.pop()
+                    self.path.reverse()
+                    self.get_next_dest()
+
             elif state=='return':
                 if self.measures.visitingLed==True:
                     self.setVisitingLed(False)
                 if self.measures.returningLed==True:
                     self.setReturningLed(False)
-                self.wander()
+                if self.measures.ground>=0 and not on_target:
+                    self.setVisitingLed(True)
+                    self.num_targets_visited += 1
+                    on_target = True
+                elif self.measures.ground < 0 and on_target:
+                    on_target = False
+                if self.start_cell[0]==self.cur_cell[0] and self.start_cell[1]==self.cur_cell[1]:
+                    self.finish()
+                else:
+                    self.decide()
             
 
     def wander(self):
@@ -93,6 +134,54 @@ class MyRob(CRobLinkAngs):
         else:
             print('Go')
             self.driveMotors(0.1,0.1)
+
+    # --------------------------------------------
+
+    def decide(self):
+        ROT_THRESHOLD = 0
+        MOVE_THRESHOLD = 0.2
+
+        angle_dif = self.dest_angle - self.measures.compass
+        
+        if abs(angle_dif) == 360:
+            angle_dif = 0
+        elif abs(angle_dif) > 180:
+            angle_dif *= -1
+        if angle_dif > ROT_THRESHOLD:
+            self.driveMotors(-0.02, 0.02)
+        elif angle_dif < ROT_THRESHOLD:
+            self.driveMotors(0.02, -0.02)
+        else:
+            # angled with destination
+            # convert next cell coordinates to gps coords
+            next_pos = (self.transform_to_gps_pos(self.dest_cell[0])+self.coords_offset[0], self.transform_to_gps_pos(self.dest_cell[1])+self.coords_offset[1])
+            if abs(self.measures.x - next_pos[0]) > MOVE_THRESHOLD or abs(self.measures.y - next_pos[1]) > MOVE_THRESHOLD:
+                self.driveMotors(0.1,0.1)
+            else:
+                self.get_next_dest()
+
+
+    def get_next_dest(self):
+
+        self.cur_cell = self.dest_cell
+        if self.path_index < len(self.path):
+            self.dest_cell = self.path[self.path_index]
+            self.path_index += 1
+
+        if self.dest_cell[0] - self.cur_cell[0] > 0:
+            self.dest_angle = 0
+        elif self.dest_cell[0] - self.cur_cell[0] < 0:
+            self.dest_angle = 180
+        elif self.dest_cell[1] - self.cur_cell[1] > 0:
+            self.dest_angle = 90
+        elif self.dest_cell[1] - self.cur_cell[1] < 0:
+            self.dest_angle = -90
+
+
+    def transform_to_gps_pos(self, axis):
+        return axis*CELL_SIDE + CELL_SIDE/2
+
+    # --------------------------------------------
 
 class Map():
     def __init__(self, filename):
